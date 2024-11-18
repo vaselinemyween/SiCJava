39a1cc7447e654f2560602ac63869e476448ddd0
vaselinemyween <115502089+vaselinemyween@users.noreply.github.com>
2024-11-18 19:57:56 +0300

    add fori

--- src/com/example/test/App.java
+++ src/com/example/test/App.java
@@ -3,5 +3,7 @@ package com.example.test;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        for (int i = 0; i < 10; i++)
+            System.out.println(i);
     }
 }
