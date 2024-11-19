b64dce43bae72101eee3544dd4f3b1c8a368840d
vaselinemyween <115502089+vaselinemyween@users.noreply.github.com>
2024-11-19 14:59:38 +0300

    add vector2d

--- src/com/example/test/App.java
+++ src/com/example/test/App.java
@@ -2,8 +2,6 @@ package com.example.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        for (int i = 0; i < 10; i++)
-            System.out.println(i);
+
     }
 }
---
+++ src/com/example/test/Vector2D.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+public class Vector2D {
+    public int X;
+    public int Y;
+    public Vector2D() {
+        int X = 1;
+        int Y = 1;
+    }
+    public Vector2D(int vX, int vY) {
+        
+    }
+}
