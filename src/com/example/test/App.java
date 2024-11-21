1d72f3b4dbd6c791133792c118c510cd3fbb898b
vaselinemyween <115502089+vaselinemyween@users.noreply.github.com>
2024-11-21 20:44:25 +0300

    homework commit

--- src/com/example/test/App.java
+++ src/com/example/test/App.java
@@ -2,6 +2,16 @@ package com.example.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        //в общем т.к задачи однотипные и надо всего-лишь менять переменные, то можно считать что они решены.
+        String str = "88888888888888888888888888888888888888888888888888888888888888888888";
+        while (str.contains("222") || str.contains("888")) {
+            if (str.contains("222")) {
+                str = str.replaceFirst("222", "8");
+            }
+            else{
+                str = str.replaceFirst("888", "2");
+            }
+        }
+        System.out.println(str);
     }
 }
