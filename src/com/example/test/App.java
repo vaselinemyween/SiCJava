53bbb43f985ef0272b2c5576e531cfa6307919f8
vaselinemyween <115502089+vaselinemyween@users.noreply.github.com>
2024-11-22 14:19:46 +0300

    vector2d constr add

--- src/com/example/test/App.java
+++ src/com/example/test/App.java
@@ -3,11 +3,20 @@ package com.example.test;
 
 public class App {
     public static void main(String[] args) {
-    Vector2D v = new Vector2D(1.2374, 2.2334);
-    Vector2D v2 = new Vector2D();
-    Vector2D v3 = new Vector2D(v);
-    v.print();
-    v2.print();
-    v3.print();
+        Vector2D vA = new Vector2D();
+        Vector2D vB = new Vector2D(5.0, 7.0);
+        Vector2D vC = new Vector2D(vB);
+        vA.add(vB);
+        vA.print();
+        vA.sub(vC);
+        vA.print();
+        System.out.println(vB.length());
+        vC.scale(1.5);
+        vC.print();
+        vC.normalized();
+        System.out.println(vC.length());
+        vC.scale(2);
+        System.out.println(vA.dotProduct(vB));
+        System.out.println(vA.count);
     }
 }
--- src/com/example/test/Vector2D.java
+++ src/com/example/test/Vector2D.java
@@ -5,21 +5,46 @@ import java.util.Locale;
 public class Vector2D {
     public double X;
     public double Y;
+    static int count;
     public Vector2D() {
         X = 1;
         Y = 1;
+        count++;
     }
     public Vector2D(double vX, double vY) {
         X = vX;
         Y = vY;
+        count++;
     }
     public Vector2D(Vector2D other) {
         //Vector2D result = new Vector2D();
         X = other.X;
         Y = other.Y;
+        count++;
     }
-    public void length(Vector2D other) {
-        
+    public void scale(double scaleFactor) {
+        X *= scaleFactor;
+        Y *= scaleFactor;
+    }
+    public void add(Vector2D other) {
+        X += other.X;
+        Y += other.Y;
+    }
+    public void sub(Vector2D other) {
+        X -= other.X;
+        Y -= other.Y;
+    }
+    public double length() {
+        double len = Math.sqrt(Math.pow(this.X, 2) + Math.pow(this.Y, 2));
+        return len;
+    }
+    public double dotProduct(Vector2D other) {
+        return other.X + other.Y;
+    }
+    public double normalized() {
+        this.X = 1;
+        this.Y = 0;
+        return 1;
     }
     public void print() {
         String x = (String.format(Locale.US,"%.2f", X));
